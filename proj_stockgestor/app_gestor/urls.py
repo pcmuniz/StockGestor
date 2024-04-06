0653a9951c6bffe792c4eba66992b95e19458ca1
Erico <erico.chen@hotmail.com>
2024-04-06 16:55:05 -0300

    Terminei de ajustar o routing e views

--- proj_stockgestor/app_gestor/urls.py
+++ proj_stockgestor/app_gestor/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns=[
     path('', views.PaginaInicialView.as_view(), name='pagina-inicial'),
-    path('registro/', views.registro, name='registro'),
-    path('logado/', views.logado, name='logado'),
-    path('cadastro_produto/', views.cadastro_produto, name='cadastro_produto'),
-    path('lista_produtos', views.lista_produtos, name='lista_produtos'),
-    path('lista_fornecedores', views.lista_fornecedores, name='lista_fornecedores'),
-    path('login/', views.login, name='login'),
+    path('registro/', views.RegistroView.as_view(), name='pagina-registro'),
+    path('login/', views.LoginView.as_view(), name='pagina-login'),
+    path('lista_produtos', views.ListaProdutosView.as_view(), name='pagina-lista_produtos'),
+    path('cadastro_produto/', views.CadastroProdutosView.as_view(), name='pagina-cadastro_produto'),
+    path('lista_fornecedores', views.ListaFornecedoresView.as_view(), name='pagina-lista_fornecedores'),
+    path('logado/', views.CadastroFornecedorView.as_view(), name='pagina-cadastro_fornecedor'),
 ]
--- proj_stockgestor/app_gestor/views.py
+++ proj_stockgestor/app_gestor/views.py
@@ -1,73 +1,91 @@
-from urllib import request
-from django.shortcuts import redirect, render, HttpResponse
+from django.shortcuts import redirect, render
 from .forms import RegistroForm
-from .models import CustomUser, Fornecedores
-from .models import Produtos
-from django.contrib.auth import authenticate, login
+from .models import CustomUser, Fornecedores, Produtos
 from django.contrib.auth.hashers import check_password
-from django.contrib.auth.models import User
 from django.views import View
 
 class PaginaInicialView(View):
     def get(self, request):
         return render(request, 'app_gestor/home.html')
 
-# TDDO: criar login()
-def registro(request):
-    if request.method == "POST":
-        form = RegistroForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("/cadastro_produto")
-        
-    else:
-        form = RegistroForm()
+class RegistroView(View):
+    # TDDO: criar login()
+    def get(self, request):
+            return render(request, 'app_gestor/registro.html')
     
-    return render(request, "app_gestor/registro.html", {"form":form})
-
-def login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        try:
-            user = CustomUser.objects.get(username=username)
-        except CustomUser.DoesNotExist:
-            user = None
-            print(user)
-        if user is not None and check_password(password, user.password):
-            return redirect('/cadastro_produto')
+    def post(self, request):
+        if request.method == "POST":
+            form = RegistroForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect("/login")
+            
         else:
-            error_message = "Credenciais inválidas. Por favor, tente novamente."
-            return render(request, 'app_gestor/login.html', {'error_message': error_message})
-    else:
+            form = RegistroForm()
+        
+        return render(request, "app_gestor/registro.html", {"form":form})
+
+class LoginView(View):
+    def get(self, request):
         return render(request, 'app_gestor/login.html')
+    
+    def post(self, request):
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            try:
+                user = CustomUser.objects.get(username=username)
+            except CustomUser.DoesNotExist:
+                user = None
+                print(user)
+            if user is not None and check_password(password, user.password):
+                return redirect('/lista_produtos')
+            else:
+                error_message = "Credenciais inválidas. Por favor, tente novamente."
+                return render(request, 'app_gestor/login.html', {'error_message': error_message})
+        else:
+            return render(request, 'app_gestor/login.html')
 
-def logado(request):
-    if request.method == "POST":
-        form = request.POST
-        fornecedor = Fornecedores(nome_empresa=form["nome_empresa"], cnpj=form["cnpj"], inscricao_estadual=form["ie"]
-                                , inscricao_municipal=form["im"], endereco=form["endereco"], uf=form["uf"]
-                                , fornecedor_email=form["email"], fornecedor_telefone=form["telefone"])
-        fornecedor.save()
-       
-    return render(request, 'app_gestor/base_logado.html')
+class ListaProdutosView(View):
+    def get(self, request):
+        produtos = Produtos.objects.all()
 
-def cadastro_produto(request):
-    if request.method == "POST":
-        form = request.POST
-        produto = Produtos(nome_produto=form["nome_produto"], ref=form["ref"], marca=form["marca"], categoria=form["categoria"], localizacao=form["localizacao"],
-                            fornecedor=form["fornecedor"], data_entrada=form["data_entrada"], validade=form["validade"], codigo=form["codigo"],
-                              codigo_barras=form["codigo_barras"], preco_compra=form["preco_compra"], descricao=form["descricao"])
-        produto.save()
+        return render(request, 'app_gestor/lista_produtos.html', {'produtos': produtos})
 
-    return render(request, 'app_gestor/cadastro_produto.html')
+class CadastroProdutosView(View):
+    def get(self, request):
+        return render(request, 'app_gestor/cadastro_produto.html')
+    
+    def post(self, request):
+        if request.method == "POST":
+            form = request.POST
+            produto = Produtos(nome_produto=form["nome_produto"], ref=form["ref"], marca=form["marca"], categoria=form["categoria"], localizacao=form["localizacao"],
+                                fornecedor=form["fornecedor"], data_entrada=form["data_entrada"], validade=form["validade"], codigo=form["codigo"],
+                                codigo_barras=form["codigo_barras"], preco_compra=form["preco_compra"], descricao=form["descricao"])
+            produto.save()
 
-def lista_produtos(request):
-    produtos = Produtos.objects.all()
+        return render(request, 'app_gestor/cadastro_produto.html')
 
-    return render(request, 'app_gestor/lista_produtos.html', {'produtos': produtos})
+class ListaFornecedoresView(View):
+    def get(self, request):
+        fornecedores = Fornecedores.objects.all()
+        return render(request, 'app_gestor/lista_fornecedores.html', {'fornecedores': fornecedores})
 
-def lista_fornecedores(request):
-    fornecedores = Fornecedores.objects.all()
+class CadastroFornecedorView(View):
+    def get(self, request):
+        return render(request, 'app_gestor/base_logado.html')
 
-    return render(request, 'app_gestor/lista_fornecedores.html', {'fornecedores': fornecedores})
+    def post(self, request):
+        if request.method == "POST":
+            form = request.POST
+            fornecedor = Fornecedores(nome_empresa=form["nome_empresa"], cnpj=form["cnpj"], inscricao_estadual=form["ie"]
+                                    , inscricao_municipal=form["im"], endereco=form["endereco"], uf=form["uf"]
+                                    , fornecedor_email=form["email"], fornecedor_telefone=form["telefone"])
+            fornecedor.save()
+        
+        return render(request, 'app_gestor/base_logado.html')
+
+
+
+
+
