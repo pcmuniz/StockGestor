e1f03c9743d515b2758b8dab021627ad5c955b8a
BrunoBMayer <brunobrmayer@gmail.com>
2024-05-05 21:05:56 -0300

    Teste 3

--- proj_stockgestor/cypress/e2e/Filtrar_.cy.js
+++ proj_stockgestor/cypress/e2e/Filtrar_.cy.js
@@ -1,17 +1,19 @@
 describe('Como usuário, eu gostaria de filtrar produtos por fornecedor', () => {
-
-  it('Na lista de produtos devem aparecer apenas os produtos do forncedor escolhido pelo usuário.', () => {
-    
+  it('Na lista de produtos devem aparecer apenas os produtos do fornecedor escolhido pelo usuário.', () => {
     cy.visit('http://127.0.0.1:8000/');
     cy.get('.login-button').click();
-      cy.wait(30); 
-      cy.get('#username').type('cesarschool');
-      cy.get('#password').type('Dj@ngo2024');
-      cy.wait(30);
-      cy.get('.btn').click();
-      cy.wait(30);
-      
+    cy.wait(30); 
+    cy.get('#username').type('cesarschool');
+    cy.get('#password').type('Dj@ngo2024');
+    cy.wait(1000);
+    cy.get('.btn').click();
+    cy.wait(1000); 
+    cy.get('.form-select').select('Apple Inc.');
+    cy.wait(2000);
+    cy.get('.row-cols-lg-auto > :nth-child(3) > .btn').click();
+
+    cy.get('thead > tr > :nth-child(1)').should('not.contain', '34344576', '34343', '34344576');
+  });
+});
 
 
-  })
-})
